cd953baa19d9a2a62dc986d4b03c85c5bb13fa5b
unknown <gasa346@gmail.com>
2024-03-16 11:32:21 +0100

    404page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    
+     function animation() {
+
+        let  windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+
+        $('.animation').each(function () {
+
+            let position = $(this).offset().top;
+            let animationName = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css('animation-delay', delay);
+            }
+        });
+    }
+    $(window).scroll(function () {
+        animation();
+    });
+    animation();
 
     //PRODUCT SLIDER
     if ($('.product-slider').length > 0) {
@@ -37,6 +59,59 @@ $(document).ready(function () {
 });
 }
 });
+
+ 
+ 
+ //VALIDATE FORM
+    if($('.contact-form').length > 0){
+    
+    $('.contact-form').validate({
+        
+        highlight: function(element){
+            $(element).addClass('is-invalid').removeClass('is-valid');
+        },
+        unhighlight:function(element){
+            $(element).addClass('is-valid').removeClass('is-invalid');
+        },
+        rules:{
+            name:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+             subject:{
+                required:true,
+                
+            },
+            message:{
+                required:true
+            }
+        },
+        messages:{
+            name:{
+                required:'The Name* field is required',
+            },
+             email:{
+                required:'The Email* field is required',
+                email: 'Please provide a vali email address'
+            },
+             subject:{
+                required:'The Subject* field is required',
+            },
+            message:{
+                required:'The Message* field is required',
+            }
+        },
+        
+        errorElement: 'p',
+          errorPlacement: function(error, element) {
+        error.appendTo( element.closest(".form-group").find(".error-msg") );
+  }
+
+     });
+   }
 
 
 
